b7f038b09ca532a6cc5b7a5cae89149dae0c85ec
rushba <rushba@corp.badoo.com>
2022-01-07 11:21:18 +0300

    add recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -59,3 +59,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='test recipe',
+            time_minutes=5,
+            price=5.00,
+        )
+
+        self.assertEquals(str(recipe), recipe.title)
